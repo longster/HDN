9e55da75497360d694c09a7a936ee4128b41482c
longster <longster@live.com>
2014-04-15 15:14:37 -0500

    More updates to event manager functions

--- modules/content-events.php
+++ modules/content-events.php
@@ -2,26 +2,28 @@
 	<?php the_content(); ?>
 </div>
 
+
+<?php /* Place this in Events > Formatting > Default Event List Format (Remove header and footer format) */ ?>
 <?php /* 
 <div class="relatedthumb box-post col-sm-6 col-md-4 col-lg-3">
-<div class="thumbnail"> 
+	<div class="thumbnail"> 
 
-<header class="box-entry-header hidden-xs">
-	#_CATEGORIES 
-</header><!-- .entry-header -->
+		<header class="box-entry-header hidden-xs">
+			#_CATEGORIES 
+		</header><!-- .entry-header -->
 
-<div class="box-entry-title">
-	#_EVENTLINK<br/>
-	<p>#_EVENTDATES<br/>
-	#_EVENTTIMES<br/>
-	{has_location}<br/><i>#_LOCATIONNAME, #_LOCATIONTOWN #_LOCATIONSTATE</i>{/has_location}</p>
-</div>
+		<div class="box-entry-title">
+			<p>#_EVENTLINK</p>
+			<p>#_EVENTDATES<br/>
+			#_EVENTTIMES<br/>
+			{has_location}<br/><i>#_LOCATIONNAME, #_LOCATIONTOWN #_LOCATIONSTATE</i>{/has_location}</p>
+		</div>
 
-<footer class="box-entry-meta hidden-xs">		
-	<a href="mailto:#_CONTACTEMAIL">#_CONTACTNAME</a>
-	<a href="#_CONTACTURL" target="_blank" class="box-entry-meta-date pull-right">#_CONTACTURL</a>
-</footer><!-- .box-entry-meta -->
+		<footer class="box-entry-meta hidden-xs">		
+			<a href="mailto:#_CONTACTEMAIL">#_CONTACTNAME</a>
+			<a href="#_CONTACTURL" target="_blank" class="box-entry-meta-date pull-right">#_CONTACTURL</a>
+		</footer><!-- .box-entry-meta -->
 
-</div>
+	</div>
 </div> 
 */ ?>
