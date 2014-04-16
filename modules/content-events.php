1fdb298ed716ff7031dd63e42681792d4aadc1ce
longster <longster@live.com>
2014-04-15 21:24:19 -0500

    Add directions in event manager configuration

--- modules/content-events.php
+++ modules/content-events.php
@@ -3,27 +3,52 @@
 </div>
 
 
-<?php /* Place this in Events > Formatting > Default Event List Format (Remove header and footer format) */ ?>
 <?php /* 
-<div class="relatedthumb box-post col-sm-6 col-md-4 col-lg-3">
-	<div class="thumbnail"> 
+	Event Manager Configuration 
+	------------------------------------------------------------------------------------------
+	CATEGORIES
+	
+	GENERAL
+	General Options
+	- NO: Enable Bookings
+	- NO: Show some love
+	- NO: Enable locaions
 
-		<header class="box-entry-header hidden-xs">
-			#_CATEGORIES 
-		</header><!-- .entry-header -->
+	Event Submission Forms
+	- YES - Allow anonymous event submissions?
+	- ID user #(198) for incognito/guest - Guest Default User
 
-		<div class="box-entry-title">
-			<p>#_EVENTLINK</p>
-			<p>#_EVENTDATES<br/>
-			#_EVENTTIMES<br/>
-			{has_location}<br/><i>#_LOCATIONNAME, #_LOCATIONTOWN #_LOCATIONSTATE</i>{/has_location}</p>
+	Styling Options
+	- NO: Search forms
+	- NO: Locations list page
+	- NO: Event booking forms
+
+	GENERAL > PAGE
+	- YES: Enable Archives
+	- YES: Include in Wordpress Searches
+
+	GENERAL > FORMAT
+	Events > Formatting > Default Event List Format (Remove header and footer format)
+	------
+	<div class="relatedthumb box-post col-sm-6 col-md-4 col-lg-3">
+		<div class="thumbnail"> 
+
+			<header class="box-entry-header hidden-xs">
+				#_CATEGORIES 
+			</header><!-- .entry-header -->
+
+			<div class="box-entry-title">
+				<p>#_EVENTLINK</p>
+				<p>#_EVENTDATES<br/>
+				#_EVENTTIMES<br/>
+				{has_location}<br/><i>#_LOCATIONNAME, #_LOCATIONTOWN #_LOCATIONSTATE</i>{/has_location}</p>
+			</div>
+
+			<footer class="box-entry-meta hidden-xs">		
+				<a href="mailto:#_CONTACTEMAIL">#_CONTACTNAME</a>
+				<a href="#_CONTACTURL" target="_blank" class="box-entry-meta-date pull-right">#_CONTACTURL</a>
+			</footer><!-- .box-entry-meta -->
+
 		</div>
-
-		<footer class="box-entry-meta hidden-xs">		
-			<a href="mailto:#_CONTACTEMAIL">#_CONTACTNAME</a>
-			<a href="#_CONTACTURL" target="_blank" class="box-entry-meta-date pull-right">#_CONTACTURL</a>
-		</footer><!-- .box-entry-meta -->
-
-	</div>
-</div> 
+	</div> 
 */ ?>
