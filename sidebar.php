a2fd42b1bff0e9c7be5819faf08f176d0ce22674
longster <longster@live.com>
2014-04-17 14:33:52 -0500

    remove header sidebars

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,14 @@
 <aside id="sidebar" class="col-md-4 col-lg-3">
 <?php if ( is_bbpress() ) : // bbpress ?>
 
-    <h2>Sidebar BBPress</h2>
     <?php dynamic_sidebar("Sidebar-Forums"); ?>
 
 <?php elseif ( is_page() ) : ?>
 
-    <h2>Sidebar Page</h2>
     <?php dynamic_sidebar("Sidebar-Page"); ?>
 
 <?php else : ?>
 
-    <h2>Sidebar Single</h2>
     <?php dynamic_sidebar("Sidebar-Single"); ?>
 
 <?php endif;?>
