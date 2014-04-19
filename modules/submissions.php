4ddb901a259e91bd996e560fc1dc966789789709
longster <longster@live.com>
2014-04-19 16:50:47 -0500

    Update submission
    Login-logo.png needs to be in wp-content folder

--- modules/submissions.php
+++ modules/submissions.php
@@ -6,24 +6,7 @@
 				<div class="well">
 					<h3>Submissions are FREE! </h3>
 					Register with us and have the options to share information, activities, and/or participate forums. If you don't want to register, take a few moments to fill out the form and submit to us. We will review your information as soon as possible.
-					<?php
-						if ( ! is_user_logged_in() ) { // Display WordPress login form:
-						    $args = array(
-						        'redirect' => admin_url(), 
-						        'form_id' => 'loginform-custom',
-						        'label_username' => __( 'Username' ),
-						        'label_password' => __( 'Password' ),
-						        'label_remember' => __( 'Remember Me' ),
-						        'label_log_in' => __( 'Log In' ),
-						        'remember' => true
-						    );
-						    wp_login_form( $args );
-						} else { // If logged in:
-						    wp_loginout( home_url() ); // Display "Log Out" link.
-						    echo " | ";
-						    wp_register('', ''); // Display "Site Admin" link.
-						}
-					?>
+					<?php wp_loginout(); ?>
 				</div>
 			</div>
 		</div>
