3450a92aba5dbaa71a9363e58e50e25314ef05f6
longster <longster@live.com>
2014-04-25 14:37:05 -0500

    Fix footer stuff.

--- footer.php
+++ footer.php
@@ -16,44 +16,52 @@
 	<div class="aside-footer">
 		<div class="container">
 			<div class="row">
-			<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer')) : ?>
-				<div class="col-md-12">
-					<p>Configure at Dashboard > Appearance > Widget > Footer</p>
-					<p>Archives, Categories, Tags, Forums List, Partners, Network, Authors, etc...</p>
+			<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer-Company')) : ?>
+				<div class="col-md-4">
+					<p>Configure at Dashboard > Appearance > Widget > Footer-Company</p>
+					<?php /* 
+					<div class="col-md-4">
+						<a href="http://echode.com"><img src="http://localhost/wp-content/themes/hdn/assets/img/echode-logo.png" class="logo-foot"/></a>
+						<p>Houston Deaf Network is created by <a href="http://echode.com">Echode</a> (formely known as BE7A, LLC), based in Houston, TX. Echode focuses on building web products, services, and websites. To learn more, visit us.</p>
+					</div>
+					*/ ?>
 				</div>
 		    <?php endif; ?>
-			</div>
-			<div class="row">
-			<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer-Company')) : ?>
-				<div class="col-md-12 hide">
-					<p>Configure at Dashboard > Appearance > Widget > Footer-Company</p>
-				</div>
-				<?php /* 
+			<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer')) : ?>
 				<div class="col-md-8">
-					<a href="#">ECHODE logo here...</a>
-					<p>HDN is made by <a href="#">ECHODE</a> (formely known as BE7A, LLC), based in Houston, Texas. We've put more than 10 years in experience building web products, services and websites into this framework.</p>
+					<p>Configure at Dashboard > Appearance > Widget > Footer</p>
+					<p>Archives, Categories, Tags, Forums List, Partners, Network, Authors, etc...</p>
+					<?php /*
+					<div class="col-md-2">
+						<h5>Archives</h5>
+						<ul class="list-unstyled">
+							<li><a href="#">Month</a></li>
+						</ul>
+					</div>
+					<div class="col-md-2">
+						<h5>Categories</h5>
+						<ul class="list-unstyled">
+							<li><a href="#">Category</a></li>
+						</ul>
+					</div>
+					<div class="col-md-2">
+						<h5>Tags</h5>
+						<ul class="list-unstyled">
+							<li><a href="#">Tag</a></li>
+						</ul>
+					</div>
+					<div class="col-md-2">
+						<h5>About</h5>
+						<ul class="list-unstyled">
+							<li><a href="#">About</a></li>
+							<li><a href="#">Contribute/a></li>
+							<li><a href="#">Advertisements</a></li>
+							<li><a href="#">Disclaimer &amp; Policy</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>
+					*/ ?>
 				</div>
-				<div class="col-md-2">
-					<h5>Title</h5>
-					<ul class="list-unstyled">
-						<li><a href="#">About</a></li>
-						<li><a href="#">Echode</a></li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="#">Advertisements</a></li>
-						<li><a href="#">Disclaimer &amp; Policy</a></li>
-					</ul>
-				</div>
-				<div class="col-md-2">
-					<h5>Title</h5>
-					<ul class="list-unstyled">
-						<li><a href="#">Contribute &amp; Submissions</a></li>
-						<li><a href="#">Events &amp; Activities</a></li>
-						<li><a href="#">Articles &amp; News</a></li>
-						<li><a href="#">Job Listing</a></li>
-						<li><a href="#">Directory</a></li>
-					</ul>
-				</div>
-				*/ ?>
 		    <?php endif; ?>
 			</div>
 		</div>
@@ -61,8 +69,6 @@
 	<div class="container credit">
 		<div class="row">
 			<div class="col-md-6">
-				<div class="row">
-					<div class="col-md-12">
 		            <?php wp_nav_menu( array(
 						'theme_location'	=>	'footer',
 						'menu_class'		=>	'list-inline',
@@ -70,14 +76,15 @@
 						'fallback_cb'		=>	'hdn_menu_fallback',
 						'walker'			=>	new hdn_Nav_Walker,
 					) );  ?>
-					</div>
-					<div class="col-md-12">
-						<?php hdn_credits(); ?>
-					</div>
-				</div>
+					<div class="copyright"><?php hdn_credits(); ?></div>
 			</div>
 			<div class="col-md-6 text-right">
-				<p>Social links</p>
+				<ul class="list-inline">
+					<li><a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-fb.png" alt="Facebook" /></a></li>
+					<li><a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-twitter.png" alt="Twitter" /></a></li>
+					<li><a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-linkedin.png" alt="LinkedIn" /></a></li>
+					<li><a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-fb.png" alt="RSS" /></a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
