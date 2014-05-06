af4025c7fcacd14be7b1d0ffce088d11f22e1f4d
longster <longster@live.com>
2014-05-06 12:51:39 -0500

    clean up

--- modules/submissions.php
+++ modules/submissions.php
@@ -5,12 +5,6 @@
 			<div class="col-md-8 col-md-offset-2">
 				<a class="btn btn-primary btn-block" href="<?php echo get_permalink(14958); ?>">Submit your information: Articles, News, Activities, Events, Jobs, or Business Directory</a>
 			</div>
-			<!--div class="col-md-5 col-md-offset-1">
-				<a class="btn btn-primary btn-block" href="<?php echo get_permalink(14958); ?>">Submit News, Jobs, or Business</a>
-			</div>
-			<div class="col-md-5">
-				<a class="btn btn-primary btn-block" href="<?php echo get_permalink(12729); ?>">Submit Activities or Events</a>
-			</div-->
 		</div>
 	</div>
 </header>
